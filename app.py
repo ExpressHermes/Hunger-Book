7a591ba38f5fb14b7128de9a479ad6ee17413f3e
Shivam Yadav <shivam@Shivams-MacBook-Pro.local>
2021-09-12 21:46:34 +0530

    form data remains filled when searching and code improvements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, redirect, request
 from final import Restaurant
 
 app = Flask(__name__)
-env_config = os.getenv('APP_SETTINGS', 'config.Config')
+env_config = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
 app.config.from_object(env_config)
 
 def get_restaurants(max_Price, people, min_Price, cuisine, locality):
@@ -29,8 +29,16 @@ def search():
         max_Price = int(request.args.get('maxBudget'))
         cuisine = request.args.get('cuisine')
         locality = request.args.get('locality')
+        # to input values into html form
+        input = {
+            'guests': people,
+            'minBudget': min_Price,
+            'maxBudget': max_Price,
+            'cuisine': cuisine,
+            'locality': locality
+        }
         restaurants = get_restaurants(max_Price, people, min_Price, [cuisine], [locality])
-        return render_template('search.html', title='Search', restaurants=restaurants)
+        return render_template('search.html', restaurants=restaurants, input=input )
     else:
         return redirect(url_for('home'))
 
